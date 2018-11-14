90bdcd3efdcf809d42412245b29a64332ebf33a8
nding1718 <nding1718@gmail.com>
2018-11-14 13:29:25 -0800

    use jap repository instead of crudrepository

--- src/main/java/ndingspringboot/BlogSite/domain/UserRepository.java
+++ src/main/java/ndingspringboot/BlogSite/domain/UserRepository.java
@@ -1,8 +1,9 @@
 package ndingspringboot.BlogSite.domain;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
