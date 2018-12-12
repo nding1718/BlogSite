3f36b1332bfee371ab18a3ac8abfb87790ab7e1b
nding1718 <nding1718@gmail.com>
2018-12-11 20:20:50 -0800

    clean and add comments to the Comments controller

--- src/main/java/ndingspringboot/BlogSite/rest/CommentController.java
+++ src/main/java/ndingspringboot/BlogSite/rest/CommentController.java
@@ -1,4 +1,112 @@
 package ndingspringboot.BlogSite.rest;
 
+import ndingspringboot.BlogSite.domain.Blog;
+import ndingspringboot.BlogSite.domain.Comment;
+import ndingspringboot.BlogSite.domain.User;
+import ndingspringboot.BlogSite.service.BlogService;
+import ndingspringboot.BlogSite.service.CommentService;
+import ndingspringboot.BlogSite.utils.ConstraintViolationExceptionHandler;
+import ndingspringboot.BlogSite.utils.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/comments")
 public class CommentController {
+
+    @Autowired
+    private BlogService blogService;
+
+    @Autowired
+    private CommentService commentService;
+
+    /**
+     * Get the comments list
+     * @param blogId
+     * @param model
+     * @return
+     */
+    @GetMapping
+    public String listComments(@RequestParam(value="blogId",required=true) Long blogId, Model model) {
+        Blog blog = blogService.getBlogById(blogId);
+        List<Comment> comments = blog.getComments();
+
+        // Determine whether this comment belong to the user
+        String commentOwner = "";
+        if (SecurityContextHolder.getContext().getAuthentication() !=null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                &&  !SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
+            User principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal !=null) {
+                commentOwner = principal.getUsername();
+            }
+        }
+
+        model.addAttribute("commentOwner", commentOwner);
+        model.addAttribute("comments", comments);
+        return "/userspace/blog :: #mainContainerRepleace";
+    }
+    /**
+     * Publish a comment
+     * @param blogId
+     * @param commentContent
+     * @return
+     */
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")  // must grand a authority for the method to be usd
+    public ResponseEntity<Response> createComment(Long blogId, String commentContent) {
+
+        try {
+            blogService.createComment(blogId, commentContent);
+        } catch (ConstraintViolationException e)  {
+            return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "Success!", null));
+    }
+
+    /**
+     * Delete comment,
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response> deleteBlog(@PathVariable("id") Long id, Long blogId) {
+
+        boolean isOwner = false;
+        User user = commentService.getCommentById(id).getUser();
+
+        // Determine if the user is the owner of the blog
+        if (SecurityContextHolder.getContext().getAuthentication() !=null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                &&  !SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
+            User principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal !=null && user.getUsername().equals(principal.getUsername())) {
+                isOwner = true;
+            }
+        }
+
+        if (!isOwner) {
+            return ResponseEntity.ok().body(new Response(false, "No Access!"));
+        }
+
+        try {
+            blogService.removeComment(blogId, id);
+            commentService.removeComment(id);
+        } catch (ConstraintViolationException e)  {
+            return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "Success!", null));
+    }
 }
+
