adfefddb0f7d49abf608e9268a54cee59a219728
nding1718 <nding1718@gmail.com>
2018-12-11 20:26:45 -0800

    add some comments

--- src/main/java/ndingspringboot/BlogSite/rest/VoteController.java
+++ src/main/java/ndingspringboot/BlogSite/rest/VoteController.java
@@ -1,4 +1,87 @@
 package ndingspringboot.BlogSite.rest;
 
+import ndingspringboot.BlogSite.domain.User;
+import ndingspringboot.BlogSite.service.BlogService;
+import ndingspringboot.BlogSite.service.VoteService;
+import ndingspringboot.BlogSite.utils.ConstraintViolationExceptionHandler;
+import ndingspringboot.BlogSite.utils.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.ConstraintViolationException;
+
+@Controller
+@RequestMapping("/votes")
 public class VoteController {
+
+    @Autowired
+    private BlogService blogService;
+
+    @Autowired
+    private VoteService voteService;
+
+    /**
+     * Create a vote
+     * @param blogId
+     * @return
+     */
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
+    public ResponseEntity<Response> createVote(Long blogId) {
+
+        try {
+            blogService.createVote(blogId);
+        } catch (ConstraintViolationException e)  {
+            return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "Vote success!", null));
+    }
+
+    /**
+     * Cancel a vote
+     * @param id
+     * @param blogId
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
+    public ResponseEntity<Response> delete(@PathVariable("id") Long id, Long blogId) {
+
+        boolean isOwner = false;
+        User user = voteService.getVoteById(id).getUser();
+
+        // check if the user is the owner of the vote
+        if (SecurityContextHolder.getContext().getAuthentication() !=null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                &&  !SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
+            User principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal !=null && user.getUsername().equals(principal.getUsername())) {
+                isOwner = true;
+            }
+        }
+
+        if (!isOwner) {
+            return ResponseEntity.ok().body(new Response(false, "Don't have access"));
+        }
+
+        try {
+            blogService.removeVote(blogId, id);
+            voteService.removeVote(id);
+        } catch (ConstraintViolationException e)  {
+            return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+        }
+
+        return ResponseEntity.ok().body(new Response(true, "Cancel success!", null));
+    }
 }
