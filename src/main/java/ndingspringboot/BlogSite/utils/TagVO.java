b484f3eefbedeb56669b7b55198fd1f63743f234
nding1718 <nding1718@gmail.com>
2018-12-11 19:40:12 -0800

    add value objects and some utility class

--- src/main/java/ndingspringboot/BlogSite/utils/TagVO.java
+++ src/main/java/ndingspringboot/BlogSite/utils/TagVO.java
@@ -1,4 +1,38 @@
 package ndingspringboot.BlogSite.utils;
 
-public class TagVO {
+import java.io.Serializable;
+
+/**
+ * Tag value object class, since we don't have a tag class, yet we still need to pass the value
+ * of some tags to the front end, so we build this value object for tags
+ */
+public class TagVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private Long count;
+
+    public TagVO(String name, Long count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
 }
+
